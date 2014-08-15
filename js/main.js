88c1494cded94fa21bb2a5ee634dafa5c3433fd9
Gajan Nagaraj <thegajannagaraj@gmail.com>
2014-08-15 15:01:15 -0700

    Upadted Some Css...

---
+++ js/main.js
@@ -0,0 +1,14 @@
+﻿$(document).ready(function () {
+    $(".primary-btn").click(function () {
+        $(".primary-btn").addClass("active");
+        $(".runoff-btn").removeClass("active");
+        $("#primary").css("display", "block");
+        $("#runoff").css("display", "none");
+    });
+    $(".runoff-btn").click(function () {
+        $(".runoff-btn").addClass("active");
+        $(".primary-btn").removeClass("active");
+        $("#primary").css("display", "none");
+        $("#runoff").css("display", "block");
+    });
+})
